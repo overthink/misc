310b6595c596dd6be0be2d93272ab92c1dc71728
Mark Feeney <mark.feeney@gmail.com>
2013-04-05 00:15:21 -0400

    Start/stop embedded server
    - use ref and dosync rather than atom for check-then-act support
    - make "join" an option with keyword params (neat)

--- compojure-test/src/compojure_test/app.clj
+++ compojure-test/src/compojure_test/app.clj
@@ -7,30 +7,39 @@
 
 (set! *warn-on-reflection* true)
 
-(defonce server (atom nil))
+; Global server reference.  Starts off nil until the server is started.  Set
+; back to nil when server stopped.
+(defonce server (ref nil))
 
-(defn config-jetty
-  "Access to the server instance before it is started."
+(defn config-server
+  "Access to the server instance before it is started.  Returns nil."
   [^Server s]
   (.setStopAtShutdown s true))
 
 (defn start-server 
-  "Useful when working in REPL."
+  "Start the embedded web server if not running.  Does nothing if server
+  already running.
+  Usage: (start-server)            <-- good for repl
+         (start-server :join true) <-- good for main"
+  [& {:keys [join] :or {join false}}]
+  (dosync
+    (when-not (ensure server)
+      (ref-set server (run-jetty #'h/dummy-handler 
+                                 {:port 3000 
+                                  :join? join
+                                  :configurator config-server})))))
+
+(defn stop-server 
+  "Initiate orderly shutdown of the server.  Blocks until done.  If already
+  shutdown, does nothing."
   []
-  ; Should check that it's not started
-  ; Should take join value as arg
-  (swap! server (fn [_] (run-jetty #'h/dummy-handler 
-                                   {:port 3000 
-                                    :join? false
-                                    :configurator config-jetty}))))
-
-(defn stop-server []
-  (let [^Server s @server]
-    (.stop s)))
-
+  (dosync
+    (when-let [^Server s (ensure server)]
+      (.stop s)
+      (ref-set server nil))))
 
 (defn -main [& args]
-  (start-server)
-  (.join @server)
+  ; start the server and block forever
+  (start-server :join true)
   (println "bye"))
 
